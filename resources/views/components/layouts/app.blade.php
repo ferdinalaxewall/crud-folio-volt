53bcc66c70912d8b7ff5115e816c9dcc266977a3
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-06-20 23:47:10 -0700

    Initial Commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<html class="bg-gray-50 text-gray-600">
+    <head>
+        <title>{{ config('app.name') }}</title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+                theme: {
+                    container: {
+                        center: true
+                        , padding: '1rem'
+                    , }
+                , }
+            }
+        
+        </script>
+    </head>
+
+    <body>
+        <div class="antialiased bg-gray-50 dark:bg-gray-900">
+            {{-- Navbar --}}
+            <x-widgets.navbar />
+            
+            <!-- Sidebar -->
+            <x-widgets.sidebar />
+    
+            <main class="p-4 md:ml-64 h-auto pt-24">
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+
+</html>
